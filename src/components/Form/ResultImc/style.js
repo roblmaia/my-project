1ad409ec6ea5e603b1abe47ef18cf12b118bf858
roblmaia <rob-lmaia@hotmail.com>
2023-11-17 19:28:32 -0300

    primeiro commit

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex: 1,
+        marginTop: 10,
+        paddingTop: 20,
+        borderRadius: 50,
+        alignItems: "center",
+        width:"100%",
+        //backgroundColor: "blue",
+    },
+    numberImc: {
+        fontSize: 48,
+        color: "#1f2a58",
+        fontWeight: "bold",
+    },
+    information: {
+        fontSize: 18,
+        color: "#1f2a58",
+        fontWeight: "bold",
+    },
+});
+export default styles
---
+++ src/components/Form/style.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    formContext: {
+        width: "100%",
+        height: "100%",
+        bottom: 0,
+        backgroundColor: "#dee0e6",
+        alignItems: "center",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        marginTop: 30,
+    },
+    form: {
+        width: "100%",
+        height: "auto",
+        marginTop: 30,
+        padding: 10,
+    },
+    formLabel: {
+        color: "#000000",
+        fontSize: 18,
+        paddingLeft: 20,
+    },
+    input: {
+        width: "90%",
+        borderRadius: 50,
+        backgroundColor: "#74baff",
+        height: 40,
+        margin: 12,
+        paddingLeft: 10,
+    },
+    buttonCalculator: {
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "90%",
+        backgroundColor: "#fde09e",
+        paddingTop: 14,
+        paddingBottom: 14,
+        marginLeft: 12,
+        margin: 30,
+    },
+    textButtonCalculator: {
+        fontSize: 20,
+        color: "#000000",
+    },
+    assinatura: {
+        fontSize: 5,
+        color: "#000000",
+    },
+    errorMessage: {
+        fontSize: 12,
+        color: "red",
+        fontWeight: "bold",
+        paddingLeft: 20,
+    }
+});
+
+export default styles
